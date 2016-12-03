f8a4f17eff2c378556333494cefd8e2c2a9214d9
Razi Syed <syed.razi@outlook.com>
2016-12-03 14:54:48 -0500

    dsauidhj

--- index.js
+++ index.js
@@ -18,13 +18,39 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-    res.send('Error, wrong token')
+    res.sendStatus(200)
 })
+
+const token = "EAAQqEd0gfagBAEnQFPEaUJCmHkENsWWiYds73n3JkSwlnSsxbi3Syi4vMy0KB4Sejg39FahW7CBzSAxuOlfBjvM7ZBB8csEFhJTSmZCZCO9IToBSst416b38ccVnERPS5iIua4pcNoWl7gcvuG77pRqksJD9ZBX1GyM7NM8ZBrQZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
