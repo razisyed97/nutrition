b972a0ca3ed88297a4dc4c9b264b866e43077b41
Razi Syed <syed.razi@outlook.com>
2016-12-03 16:01:48 -0500

    dsadoi

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0]
     console.log(messaging_events.attachments)
     console.log("Length: ", req.body.entry.length)
     for (let i = 0; i < messaging_events.length; i++) {
